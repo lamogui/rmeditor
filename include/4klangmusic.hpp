0f397db373b72eadbca81cdb286bc4dbc9b02686
lamogui <julien.deloor@gmail.com>
2017-05-06 14:23:16 +0200

    test si git est intelligent

---
+++ include/4klangmusic.hpp
@@ -0,0 +1,44 @@
+﻿
+#ifndef _4KLANGMUSIC_HPP
+#define _4KLANGMUSIC_HPP
+
+#include "music.hpp"
+#include "system.hpp"
+#include "tf4.hpp"
+#include <thread>
+
+class _4KlangMusic : public Music
+{
+  Q_OBJECT
+
+public:
+  _4KlangMusic(const QString& filename, double length, QDomNode node ,LogWidget& log,QObject* parent);
+  virtual ~_4KlangMusic();
+
+  virtual double getTime() const;
+
+  virtual void exportMusicCData(const QFile& source, const QFile& header) const;
+
+  /*
+    RtAudio stuff
+  */
+  virtual bool createRtAudioStream();
+  virtual void processAudio(void *outputBuffer, unsigned int nBufferFrames,
+                            double streamTime, RtAudioStreamStatus status);
+
+
+public slots:
+  virtual bool load();
+  virtual void setPosition(double time);
+  virtual void updateTextures();
+
+protected:
+  float* _buffer; //Song buffer
+  uint32_t _nbSamples; //size in "2.*float" of the buffer
+  uint32_t _bufferPos; //Position in "float" into the buffer
+  bool _ended; //Fetch reached the end...
+  QMutex _mutex;
+  std::thread _thread;
+};
+
+#endif
